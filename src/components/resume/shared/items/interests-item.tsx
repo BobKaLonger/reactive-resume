31d17e37ad435f1422eae63ae57c258df13950b9
BobKaLonger <rkollingeriii@gmail.com>
2026-04-27 16:53:09 -0400

    update interests section

--- src/components/resume/shared/items/interests-item.tsx
+++ src/components/resume/shared/items/interests-item.tsx
@@ -1,5 +1,7 @@
 import type { SectionItem } from "@/schema/resume/data";
 
+import { TiptapContent } from "@/components/input/rich-input";
+import { stripHtml } from "@/utils/string";
 import { cn } from "@/utils/style";
 
 import { PageIcon } from "../page-icon";
@@ -11,27 +13,25 @@ type InterestsItemProps = SectionItem<"interests"> & {
 export function InterestsItem({ className, ...item }: InterestsItemProps) {
   return (
     <div className={cn("interests-item", className)}>
-      {/* Header */}
-      <div className="section-item-header interests-item-header flex items-center gap-x-1.5">
-        <PageIcon icon={item.icon} className="section-item-icon interests-item-icon" />
-        <strong className="section-item-title interests-item-name">{item.name}</strong>
-      </div>
+    {/* Header */}
+    <div className="section-item-header interests-item-header flex items-center gap-x-1.5">
+    <PageIcon icon={item.icon} className="section-item-icon interests-item-icon" />
+    <strong className="section-item-title interests-item-name">{item.name}</strong>
+    </div>
 
-      {/* Keywords */}
-      {item.keywords.length > 0 && (
-        <span className="section-item-keywords interests-item-keywords inline-block opacity-80">
-          {item.keywords.join(", ")}
-        </span>
-      )}
+    {/* Keywords */}
+    {item.keywords.length > 0 && (
+      <span className="section-item-keywords interests-item-keywords inline-block opacity-80">
+      {item.keywords.join(", ")}
+      </span>
+    )}
 
-      {/* Description */}
-      {item.description && (
-        <div
-          className="section-item-description interests-item-description opacity-80"
-          // eslint-disable-next-line react/no-danger
-          dangerouslySetInnerHTML={{ __html: item.description }}
-        />
-      )}
+    {/* Description */}
+    <div
+    className={cn("section-item-description interests-item-description", !stripHtml(item.description) && "hidden")}
+    >
+    <TiptapContent content={item.description} />
+    </div>
     </div>
   );
 }
